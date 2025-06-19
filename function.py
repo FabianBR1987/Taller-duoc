70b60f1d176f5144517e1ee2f4b0f0728407dbf1
Jonas Mena <jon.mena@duocuc.cl>
2025-06-18 22:12:22 -0400

    feat/jonas: creo las funciones_buscar contactos y eliminar_contactos

--- function.py
+++ function.py
@@ -1,1 +1,32 @@
-###function
+###function 
+# 1.	Agregar un contacto: nombre, teléfono, email. X
+# 3.	Buscar un contacto por nombre.
+# 4.	Eliminar un contacto.
+import os
+os.system("cls")
+# Lista de contactos: cada contacto es una lista [nombre, telefono]
+# def buscar_contacto(contacto):
+contacto = ["jonas", 1234, "jonas@gmail.com" ]
+
+# def buscar_contacto(contacto):
+#     if len (contacto) == 0:
+#         print ("No hay contacto registrado")
+#     else:
+#         nombre_busqueda = input ("Ingrese el nombre a buscar")
+#     for i, nombre_busqueda in enumerate (contacto):
+#         if contacto[i].lower() == nombre_busqueda.lower():
+#             print (contacto)
+#             os.system("pause")
+#             return contacto
+#     return None
+
+# def eliminar (contacto):
+    
+if len (contacto) == 0:
+    print ("No hay contacto registrado")
+else:
+    nombre_busqueda = input ("Ingrese el nombre a buscar para eliminar")
+for nombre_busqueda in contacto:
+    contacto.pop(contacto)
+    
+    print (contacto)
